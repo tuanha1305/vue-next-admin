c8381645447975748d510d0d70a0e8fea20cc724
lyt-Top <1105290566@qq.com>
2021-06-19 19:00:44 +0800

    'admin-21.06.19:同步gitee上代码'

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,6 +8,18 @@ import tagsViewRoutes from '/@/store/modules/tagsViewRoutes.ts';
 import userInfos from '/@/store/modules/userInfos.ts';
 import requestOldRoutes from '/@/store/modules/requestOldRoutes.ts';
 
+// const modulesFiles: Record<string, Function> = import.meta.glob('./modules/*.ts');
+
+// npm run build
+// https://github.com/vitejs/vite/issues/3035
+// Top-level await is not available in the configured target environment ("chrome87", "edge88", "es2019", "firefox78", "safari13.1")
+// let modules: ModuleTree<object> = {};
+// for (const path in modulesFiles) {
+// 	const moduleName: string = path.replace(/(.*\/)*([^.]+).*/gi, '$2');
+// 	let module: ModuleTree<object> = await modulesFiles[path]();
+// 	modules = { ...modules, [moduleName]: module.default };
+// }
+
 export const key: InjectionKey<Store<RootStateTypes>> = Symbol();
 
 export const store = createStore<RootStateTypes>({
